921b0bb7e69e82f6e692024d8471870de17b17d1
Alejandro Santiago <lesthath_@gmail.com>
2024-03-29 18:58:28 -0600

    Javascript

---
+++ 1_Variables.js
@@ -0,0 +1,19 @@
+// Variables 
+
+var producto = "Audifonos Gamer";
+
+var producto1 = "computadora",
+    disponible1 = true,
+    categoria = "computadoras";
+
+// var 1disponible;
+var disponible_;
+
+// Estilos para las variables
+
+var nombre_producto = "Monitor HD" //Underscore
+var nombreProducto = "Monitor HD" //Camelcase
+var NombreProducto = "Monitor HD" //Pascal case
+var nombreproducto = "Monitor HD" //
+
+console.log(producto);
