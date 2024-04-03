7c176a40200fbe11b23f44d88bedb1b41ca3c754
Alejandro Santiago <lesthath_@hotmail.com>
2024-04-02 19:17:33 -0600

    Agregando archivos

--- IntroduccionJS/Javascript/31_notification_API.js
+++ IntroduccionJS/Javascript/31_notification_API.js
@@ -1,6 +1,15 @@
-const boton = document.querySelector("#boton");
+const boton = document.querySelector("#boton1");
 
-boton.addEventListener("clic", () => {
+boton.addEventListener("click", function () {
     Notification.requestPermission()
-})
+        .then(resultado => console.log( `El resultado es ${resultado}`))
 
+});
+
+if (Notification.permission == "granted")  {
+    new Notification ("Esta es una notificación", {
+        icon : "/img/le.png",
+        body : "Lechuga"
+    })
+        
+} 
