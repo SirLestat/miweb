4f66d5b2f30582ea520be6fd58bb10abf5595635
Alejandro Santiago <lesthath_@hotmail.com>
2024-04-10 19:08:49 -0600

    Nuevos

---
+++ IntroduccionJS/JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,16 @@
+console.log("Funciona correctamente")
+
+//Queryselector
+const heading = document.querySelector(".header__texto h2") //Retorna 1 o 0 elementos
+    console.log(heading);
+
+
+
+//querySelectorAll
+
+
+
+
+
+
+//getElementsById
