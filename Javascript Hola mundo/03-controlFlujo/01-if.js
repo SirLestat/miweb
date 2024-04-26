a33d527d4c2e898c38630b5f64b933a315fffa77
Alejandro Santiago <lesthath_@hotmail.com>
2024-04-26 16:23:44 -0600

    se agregan nuevos archivos al curso hola mundo

---
+++ Javascript Hola mundo/03-controlFlujo/01-if.js
@@ -0,0 +1,10 @@
+//  Sintaxis
+// if (condicion) { expresion } ;
+
+let edad = 15;
+
+if (edad >= 18) {
+    console.log('es mayor de edad');
+}
+
+
---
+++ Javascript Hola mundo/03-controlFlujo/02-else.js
@@ -0,0 +1,5 @@
+let edad = 15;
+
+if (edad >= 18) {
+    console.log('es mayor de edad');
+}
