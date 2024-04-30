1d8a1c6e76b546a75ab4597f852f9b2bebc6797f
Alejandro Santiago <lesthath_@hotmail.com>
2024-04-29 19:49:05 -0600

    nuevas lecciones

---
+++ Javascript Hola mundo/03-controlFlujo/03-while.js
@@ -0,0 +1,15 @@
+// Cuales son los numeros pares
+
+let i = 1;
+
+while (i <= 10) {
+    if (i % 2 === 0) {
+       
+        console.log(`${i} Es un número par`);
+        
+    }
+    
+    i++;
+}
+
+console.log('Fuera del while');
---
+++ Javascript Hola mundo/03-controlFlujo/05-do-while.js
@@ -0,0 +1,19 @@
+let i = 1;
+
+// while (i <= 10) {
+//     if (i % 2 === 0) {
+       
+//         console.log(`${i} Es un número par`);
+        
+//     }
+    
+//     i++;
+// }
+
+
+do {
+    if (i <= 10); {
+    console.log('Numero par', i)
+}
+i++;
+} while (i<10);
